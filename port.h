120184f18d82c2938174d7474f10232aeeffc7af
Honghao Zeng <nat@nat.moe>
2019-04-23 15:41:17 -0400

    code: comment

--- port.h
+++ port.h
@@ -7,10 +7,20 @@
 class Port {
 public:
     Port();
+
+    // create a port that binds to file descriptor fd
     Port(int fd); 
+
+    // read implementation
     virtual ssize_t Read(uint8_t *buffer, size_t len);
+
+    // write implementation
     virtual ssize_t Write(const uint8_t *buffer, size_t len);
+
+    // close implementation
     virtual int Close();
+
+    // get a unique port id
     virtual int GetId(void) const;
     virtual ~Port() {}
 private:
--- switch.h
+++ switch.h
@@ -10,10 +10,16 @@
 
 class Switch {
 public:
+    // plug a port into switch
     void Plug(Port *port);
+
+    // unplug a port from switch
     void Unplug(Port *port);
 private:
+    // forward traffic to other ports
     void Forward(Port *src_port, const uint8_t *buffer, size_t buf_len);
+
+    // listen on a port for incoming traffic
     void Listener(Port *port);
     std::vector<FdbEntry> fdb;
     std::mutex mtx;
