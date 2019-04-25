28dbba58b17c2c9ec9597e4b3d9a8433dd677818
Honghao Zeng <nat@nat.moe>
2019-04-25 13:52:48 -0400

    pam-auth: proper error checks & cleanup

--- server/pam-auth.cc
+++ server/pam-auth.cc
@@ -1,30 +1,61 @@
 #include "pam-auth.h"
 
-static int _panconv(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
-    char *pass = (char *) malloc(strlen((char *) appdata_ptr)+1);
-    strcpy(pass, (char *) appdata_ptr);
+static int do_conv (int num_msg, const struct pam_message **msgs, struct pam_response **resp, void *password_ptr) {
+    char *pass = (char *) malloc(strlen((char *) password_ptr) + 1);
+    strcpy(pass, (char *) password_ptr);
 
-    int i;
+    const struct pam_message *m = *msgs;
+    struct pam_response *r;
 
-    *resp = (pam_response *) calloc(num_msg, sizeof(struct pam_response));
+    if (num_msg <= 0 || num_msg >= PAM_MAX_NUM_MSG) {
+        *resp = NULL;
+        return PAM_CONV_ERR;
+    }
 
-    for (i = 0; i < num_msg; ++i) {
-        if (msg[i]->msg_style != PAM_PROMPT_ECHO_OFF) continue;
-        resp[i]->resp = pass;
+    if ((*resp = r = (struct pam_response *) calloc(num_msg, sizeof (struct pam_response))) == NULL)
+        return PAM_BUF_ERR;
+
+    for (int i = 0; i < num_msg; i++) {
+        if (m->msg == NULL) goto do_clean;
+    
+        r->resp = NULL;
+        r->resp_retcode = 0;
+
+        if (m->msg_style == PAM_TEXT_INFO) continue;
+        if (m->msg_style != PAM_PROMPT_ECHO_OFF) goto do_clean;
+        if (strcmp("Password: ", m->msg) != 0) goto do_clean;
+        r->resp = pass;
+
+        m++;
+        r++;
     }
 
     return PAM_SUCCESS;
+
+do_clean:
+    if (*resp == NULL) return PAM_CONV_ERR;
+    r = *resp;
+
+    for (int i = 0; i < num_msg; i++, r++) {
+        if (r->resp) {
+            memset(r->resp, 0, strlen(r->resp));
+            free(r->resp);
+            r->resp = NULL;
+        }
+    }
+    free(*resp);
+    *resp = NULL;
+    return PAM_CONV_ERR;
 }
 
 bool do_auth (const char *user, const char *pass) {
-    struct pam_conv conv = { &_panconv, (void *) pass };
+    struct pam_conv conv = { &do_conv, (void *) pass };
 
     pam_handle_t *handle;
-    int authResult;
 
-    pam_start("shutterd", user, &conv, &handle);
-    authResult = pam_authenticate(handle, PAM_SILENT|PAM_DISALLOW_NULL_AUTHTOK);
-    pam_end(handle, authResult);
+    if (pam_start("vswitch-tls-port-enum", user, &conv, &handle) != PAM_SUCCESS) return false;
+    int ret = pam_authenticate(handle, PAM_SILENT | PAM_DISALLOW_NULL_AUTHTOK);
+    pam_end(handle, ret);
 
-    return (authResult == PAM_SUCCESS);
+    return ret == PAM_SUCCESS;
 }
